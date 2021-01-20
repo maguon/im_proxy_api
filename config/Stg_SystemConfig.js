39d63dadbd9be2b2be0916ffc8e177c07f9f318a
yingmei567 <yingmei.yan@myxxjs.com>
2021-01-20 13:24:10 +0800

    Add ejabberd info.

--- config/Stg_SystemConfig.js
+++ config/Stg_SystemConfig.js
@@ -4,7 +4,7 @@ const loggerConfig = {
         console: { type: 'console' } ,
         file : {
             "type": "file",
-            "filename": "../hy_engine_api.html",
+            "filename": "../im_proxy_api.html",
             "maxLogSize": 2048000,
             "backups": 10
         }
@@ -12,8 +12,11 @@ const loggerConfig = {
     categories: { default: { appenders: ['console','file'], level: 'debug' } }
 }
 
-const mongooseim = {
-    admin :'http://localhost:8088',
-    client :'http://localhost:8089'
+const ejabberd = {
+    admin :'http://im.myxxjs.com:5280',
+    client :'http://im.myxxjs.com:5280',
+    adminname :'admin@im.myxxjs.com',
+    password :'123456'
 }
-module.exports = { loggerConfig, logLevel,mongooseim}
+
+module.exports = { loggerConfig, logLevel,ejabberd}
